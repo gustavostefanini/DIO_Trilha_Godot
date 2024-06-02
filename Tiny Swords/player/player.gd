a4f75f27f9ba616cf279bbdcfb2335d486e6b742
gustavostefanini <gustavo.stefanini7@gmail.com>
2024-06-02 12:59:27 -0300

    transferindo entre computadores durante implementação de dash no player.gd

--- Tiny Swords/player/player.gd
+++ Tiny Swords/player/player.gd
@@ -19,11 +19,16 @@ func _physics_process(delta: float):
 	#muda animação para correr e vice-versa
 	if input_vector != Vector2(0,0):
 		animation_player.play("run")
+		is_running = true
 	else:
 		animation_player.play("idle")
+		is_running = false
 	
 	#flipa a sprite conforme direção do movimento
 	if input_vector.x > 0:
 		sprite.flip_h = false
 	if input_vector.x < 0: 
 		sprite.flip_h = true
+	
+	#implementa dashing com shift
+	
