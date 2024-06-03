8d237981c5a677fdaedf4684296472a800d84217
Gustavo Stefanini Mac <gustavo.stefanini7@gmail.com>
2024-06-03 17:14:48 -0300

    Implementando ataques, trocando de computador

--- Tiny Swords/player/player.gd
+++ Tiny Swords/player/player.gd
@@ -1,34 +1,41 @@
 extends CharacterBody2D
-
-
-
+	 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
+var input_vector: Vector2
+var target_velocity: Vector2
 var is_running: bool = false
 var is_dashing: bool = false
 var dash_time: float = 0.0
-@export var dash_maximum_time: float = .2
-@export var dash_speed: float = 15
-@export var normal_speed: float = 5
+var dash_maximum_time: float = .2
+var dash_speed: float = 15
+var normal_speed: float = 5
 var speed = normal_speed
+var is_attacking: bool = false
+var attack_time: float = 0.0
+var attack_maximum_time: float = 0.6
+var attack_speed: float = 0
+var attack_direction = ""
+
 
 func _physics_process(delta: float):
 	
 	#movimento geral
-	var input_vector = Input.get_vector("move_left","move_right","move_up","move_down",.15)
-	var target_velocity = input_vector * speed * 100
+	input_vector = Input.get_vector("move_left","move_right","move_up","move_down",.15)
+	target_velocity = input_vector * speed * 100
 	velocity = lerp(velocity,target_velocity,.15)
 	move_and_slide()
 	
-	#muda animação para correr e vice-versa
-	if input_vector != Vector2(0,0):
-		if not is_dashing:
-			animation_player.play("run")
-		is_running = true
-	else:
-		animation_player.play("idle")
-		is_running = false
+	#configura animação para correr e respirar
+	if not is_attacking:
+		if input_vector != Vector2(0,0):
+			if not is_dashing:
+				animation_player.play("run")
+			is_running = true
+		else:
+			animation_player.play("idle")
+			is_running = false
 	
 	#flipa a sprite conforme direção do movimento
 	if input_vector.x > 0:
@@ -36,8 +43,10 @@ func _physics_process(delta: float):
 	if input_vector.x < 0: 
 		sprite.flip_h = true
 	
-	#implementa dashing com shift
+	#configura o dash
 	if is_running and Input.is_action_just_pressed("dash"):
+		if is_dashing:
+			return
 		is_dashing = true
 		speed = dash_speed
 		animation_player.play("dashing")
@@ -48,6 +57,21 @@ func _physics_process(delta: float):
 			speed = normal_speed
 			dash_time = 0.0
 			animation_player.play("idle")
+	
+	#configura o ataque
+	if Input.is_action_just_pressed("attack"):
+		if is_attacking:
+			return
+		is_attacking = true
+		speed = attack_speed
+		if input_vector.x !=  0:
+			attack_direction = "side"
 			
-		
-		
+			animation_player.play("attack_side_1")
+	if is_attacking:
+		attack_time += delta
+		if attack_time >= attack_maximum_time:
+			is_attacking = false
+			speed = normal_speed
+			attack_time = 0.0
+			animation_player.play("idle")
